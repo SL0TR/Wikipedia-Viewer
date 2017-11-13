b078774d74c5ca141a980552a9ded43bfbc7f6ae
SL0TR <yomaimsj@gmail.com>
2017-11-13 17:37:31 +0600

    Finished the initial project

--- index.js
+++ index.js
@@ -1,65 +1,105 @@
 $(document).ready(function(){
-function getData() {
+
+// A FUNCTION TO FETCH THE JSON DATA FROM THE WIKIPEDIA API WITH CALLBACK FUNCTION 
+function getData(domUpdateSearchBox, domUpdateBoxParagraph, domUpdateFromAPI) {
+
   userInput = $('.search-input-box').val();
-  console.log(userInput);
-  var link = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=5&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + "what" + "&callback=JSON_CALLBACK";
+  api = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";  
+  callBack = "&callback=JSON_CALLBACK";
+  
   $.ajax({    
-        url: link,
+        url: (api + userInput + callBack),
         type: 'GET',
         dataType: 'jsonp',
         success: function(a) {
-          pageJsonData = a.query.pages;
+          pageJsonData = a['query']['pages'];
           keyArr = [];
           for (var key in pageJsonData) {
             keyArr.push(key);
           }
-         
+          domUpdateSearchBox();
+          domUpdateBoxParagraph();
+          domUpdateFromAPI();
+
+          
+          for(i=0; i < keyArr.length; i++) {
+
+            searchResultBox = $( ".search-result-box" );
+            page="https://en.wikipedia.org/?curid=";
+            pageID = pageJsonData[keyArr[i]].pageid;
+            $( ".search-result-box" ).eq( i ).attr("href", page + pageID);
+
+          }
         },
         error: function() { console.log('Failed!'); },
     });    
 };
 
-$('.search-submit-btn').click(function (){
-  getData();
-  domUpdateSearchBox();
-  domUpdateBoxParagraph ();
-  domUpdateFromAPI();
-  userInput = $('.search-input-box').val();
-  console.log(userInput);
 
-  $(".search-submit-btn").attr("disabled", true);
+// DETECTING ENTER KEY AS SUBMISSION OF A QUERY
+$(".search-input-box").keyup(function(event) {
+  if (event.keyCode === 13) {
+      $(".search-submit-btn").click();
+      $( ".search-result-box" ).remove();
+  }
+});
+
+// INVOKING THE API CALL AND DOM MANUPULATIONS WITH THE CLICK EVENT
+$('.search-submit-btn').click(function (){
+
+  getData(domUpdateSearchBox, domUpdateBoxParagraph, domUpdateFromAPI);
+
+  $('.container-search-and-btn').animate({top: '0em'}, 1000);
+
+  $( ".search-result-box" ).remove();
+
+  
+
+});
+
+// NEW WINDOW WITH THE RANDOM ARTICLE API
+$('.get-random-page-btn').click(function (){
+  
+  window.open('https://en.wikipedia.org/wiki/Special:Random', '_blank');
+
 });
 
 
-
-getData();
-
+// DOM MANUPULATION ACCORDING TO THE API CALL
 function domUpdateFromAPI() {
   $('.search-result-title').append(pageJsonData[keyArr[0]].title);
   
             for(i=0; i < keyArr.length; i++) {
   
-              var searchResultBox = $('.search-result-desc');
-              searchResultBox[i].append(pageJsonData[keyArr[i]].extract);
+              searchResultDesc = $('.search-result-desc');
+              searchResultDesc[i].append(pageJsonData[keyArr[i]].extract);
+
+              
   
             }
 
 
 }
 
-
+// DOM MANUPULATION TO CREATE ELEMENTS AS THE QUERY
 function domUpdateSearchBox () {
 
-  for (i=0; i < keyArr.length; i++) { 
-  $( "<div class='search-result-box'>" ).appendTo( ".search-result-container" );
+  for (i=0; i < keyArr.length; i++) {
+
+  $( "<a class='search-result-box' href='#' target='_blank'></a>" ).appendTo( "section" );
   
   }
  
 }
 
+
+
+
+// CREATING NEW HTML FOR TITLE AND DESCRIPTION OF THE QUERY
 function domUpdateBoxParagraph () {
   $( "<p class='search-result-title'></p><p class='search-result-desc'></p>" ).appendTo( ".search-result-box");
 
 }
+
 
 });
