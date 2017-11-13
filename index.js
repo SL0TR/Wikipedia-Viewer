ea663f1e1d1575958020b1f07c5b04e1c7ce88c2
SL0TR <yomaimsj@gmail.com>
2017-11-13 12:23:43 +0600

    shifted from Vanilla JS to jquery due to  CORS issues

--- index.js
+++ index.js
@@ -1,47 +1,65 @@
-// Here begins the javascript code 
-
-// ES6 way to fetch api data and convert it to json 
+$(document).ready(function(){
 function getData() {
-  fetch("https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=4&prop=pageimages|" +
-  "extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=what")
-    .then(function (res) {
-      res.json()
-        .then(resp=>{
-          pageData = resp.query.pages;
-          var keyArr = [];
-          for (var key in pageData) {
+  userInput = $('.search-input-box').val();
+  console.log(userInput);
+  var link = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=5&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + "what" + "&callback=JSON_CALLBACK";
+  $.ajax({    
+        url: link,
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function(a) {
+          pageJsonData = a.query.pages;
+          keyArr = [];
+          for (var key in pageJsonData) {
             keyArr.push(key);
-            console.log(key);
-            domUpdateDiv();            
           }
-          domUpdateP();
-          console.log(pageData[keyArr[0]]);
-                 
-        })
-    })
-    .catch(function (err){
-      console.log(err)
-    })  
-}
+         
+        },
+        error: function() { console.log('Failed!'); },
+    });    
+};
+
+$('.search-submit-btn').click(function (){
+  getData();
+  domUpdateSearchBox();
+  domUpdateBoxParagraph ();
+  domUpdateFromAPI();
+  userInput = $('.search-input-box').val();
+  console.log(userInput);
+
+  $(".search-submit-btn").attr("disabled", true);
+});
+
+
 
 getData();
 
+function domUpdateFromAPI() {
+  $('.search-result-title').append(pageJsonData[keyArr[0]].title);
+  
+            for(i=0; i < keyArr.length; i++) {
+  
+              var searchResultBox = $('.search-result-desc');
+              searchResultBox[i].append(pageJsonData[keyArr[i]].extract);
+  
+            }
 
-
-function domUpdateDiv() {
-  var div= document.createElement("div"); 
-  div.setAttribute("class", "search-result-box");
-  document.querySelector(".search-result-container").appendChild(div);  
-}
-
-function domUpdateP () {
-  var p1 = document.createElement("p");
-  var p2 = document.createElement("p");
-  p1.setAttribute("class", "search-result-title");
-  p2.setAttribute("class", "search-result-desc");
-  document.getElementsByClassName("search-result-box")[0].appendChild(p1);
-  document.getElementsByClassName("search-result-box")[0].appendChild(p2);
 
 }
 
-// console.log(document.querySelector(".search-result-box").length);
+
+function domUpdateSearchBox () {
+
+  for (i=0; i < keyArr.length; i++) { 
+  $( "<div class='search-result-box'>" ).appendTo( ".search-result-container" );
+  
+  }
+ 
+}
+
+function domUpdateBoxParagraph () {
+  $( "<p class='search-result-title'></p><p class='search-result-desc'></p>" ).appendTo( ".search-result-box");
+
+}
+
+});
